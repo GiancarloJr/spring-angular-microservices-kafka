935cacac70071249dfa1fd207d98fc6a103b8eb5
GiancarloJr <108488305+GiancarloJr@users.noreply.github.com>
2023-02-03 00:24:16 -0300

    create UserController Methods

--- user-service-api/src/main/java/com/helpdesk/userserviceapi/controllers/UserController.java
+++ user-service-api/src/main/java/com/helpdesk/userserviceapi/controllers/UserController.java
@@ -1,11 +1,24 @@
 package com.helpdesk.userserviceapi.controllers;
 
 import models.requests.UserRequest;
+import models.responses.UserResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 public interface UserController {
 
+    @PostMapping
     ResponseEntity<Void> save(@RequestBody UserRequest userRequest);
+
+    @GetMapping
+    ResponseEntity<List<UserResponse>> findAll();
+
+    @GetMapping("/{id}")
+    ResponseEntity<UserResponse> findById(@PathVariable("id") Long id);
+
+    @PatchMapping("/{id}")
+    ResponseEntity<UserResponse> update(@PathVariable("id") Long id, @RequestBody UserRequest userRequest);
 
 }
